7af3bddaa43149e1644e9baa26da718b1114d3b1
viewsharp <viewsharp@yandex.ru>
2017-10-20 23:27:51 +0300

    json_server

--- bender_pcv/urls.py
+++ bender_pcv/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from json_server.views import data_json
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^data.json$', data_json)
 ]
---
+++ json_server/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JsonServerConfig(AppConfig):
+    name = 'json_server'
